63bb2953d02e098163666eea8b83aa4d8346ea25
Root <sam.pugh@dbpixelhouse.com>
2020-04-08 14:43:30 +0100

    started with just using jobs to create a moving object

---
+++ ECS_Course/Assets/OOP/Spawn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject instance;
+
+    [SerializeField]
+    int instances = 10000;
+
+    void Start()
+    {
+        for (int i = 0; i < instances; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-50, 50), 0, Random.Range(-50, 50));
+            Instantiate(instance, pos, Quaternion.identity);
+        }
+    }
+}
