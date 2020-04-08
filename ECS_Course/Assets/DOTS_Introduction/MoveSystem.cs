46fa328a88871147cabb8079b74b44235653a169
Root <sam.pugh@dbpixelhouse.com>
2020-04-08 17:20:34 +0100

    moved files so i can structure this project in to lectures

---
+++ ECS_Course/Assets/DOTS_Introduction/MoveSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class MoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        JobHandle jobHandle = Entities.WithName("MoveSystem")
+            .ForEach((ref Translation pos, ref Rotation qua) =>
+            {
+                pos.Value += new float3(0, 0.1f, 0);
+                if (pos.Value.y > 100)
+                {
+                    pos.Value.y = 0;
+                }
+            }).Schedule(inputDeps);
+
+        return jobHandle;
+    }
+}
