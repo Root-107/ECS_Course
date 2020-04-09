ee60be7470fea20823fac417ad8b7e603d2895be
Root <sam.pugh@dbpixelhouse.com>
2020-04-09 10:19:09 +0100

    Debugging entities. also found out that because the script from lecture 1 moves all entities with a transform and rotation component, had to comment out scripts from lecture 1 because it runs from the assets folder on any entities. So when creating entities in lecture 2 in the same project. this code executes on them.

--- ECS_Course/Assets/DOTS_Introduction/SpawnManager.cs
+++ ECS_Course/Assets/DOTS_Introduction/SpawnManager.cs
@@ -1,31 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Unity.Entities;
-using Unity.Mathematics;
-using Unity.Transforms;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using Unity.Entities;
+//using Unity.Mathematics;
+//using Unity.Transforms;
 
-public class SpawnManager : MonoBehaviour
-{
+//public class SpawnManager : MonoBehaviour
+//{
 
-    EntityManager manager;
+//    EntityManager manager;
 
-    public GameObject itemPrefab;
+//    public GameObject itemPrefab;
 
-    const int count = 15000;
+//    const int count = 15000;
 
-    void Start()
-    {
-        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
-        GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
-        Entity prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(itemPrefab, settings);
+//    void Start()
+//    {
+//        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+//        GameObjectConversionSettings settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+//        Entity prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(itemPrefab, settings);
 
-        for (int i = 0; i < count; i++)
-        {
-            Entity instace = manager.Instantiate(prefab);
-            Vector3 position = transform.TransformPoint(new float3(UnityEngine.Random.Range(-50, 50), UnityEngine.Random.Range(0, 100), UnityEngine.Random.Range(-50, 50)));
-            manager.SetComponentData(instace, new Translation { Value = position });
-            manager.SetComponentData(instace, new Rotation { Value = new quaternion(0,0,0,0) });
-        }
-    }
-}
+//        for (int i = 0; i < count; i++)
+//        {
+//            Entity instace = manager.Instantiate(prefab);
+//            Vector3 position = transform.TransformPoint(new float3(UnityEngine.Random.Range(-50, 50), UnityEngine.Random.Range(0, 100), UnityEngine.Random.Range(-50, 50)));
+//            manager.SetComponentData(instace, new Translation { Value = position });
+//            manager.SetComponentData(instace, new Rotation { Value = new quaternion(0,0,0,0) });
+//        }
+//    }
+//}
